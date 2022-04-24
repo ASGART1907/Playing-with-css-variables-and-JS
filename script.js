3c966e810bd3b9a846cdc6a820dfa0a8b3232161
Alisan <Alisan.Dogann.1907@gmail.com>
2022-04-24 21:30:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll(".container input");
+
+function handleUpdate(){
+  console.log(this.name);
+  const suffix = this.dataset.sizing || "";
+
+  document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener("change",handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove",handleUpdate));
